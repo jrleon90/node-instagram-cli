9548d5f9321f51a0e582836aa1fa295c3d18371e
Jose <jrleon90@gmail.com>
2018-07-02 15:34:21 -0500

    :zap: Set logic and first command

--- command.js
+++ command.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const {prompt} = require('inquirer');
+const {exportFollowersData} = require('./logic');
 
 console.log(__dirname);
 
@@ -9,4 +10,12 @@ program
     .version('0.0.1')
     .description('Instagram data retrieve')
 
+
+program
+    .command('exportData <instagramUser>')
+    .alias('e')
+    .description('Export followers data to a CSV file')
+    .action(function(instagramUser){
+        exportFollowersData(instagramUser);
+    });
 program.parse(process.argv);
--- logic.js
+++ logic.js
@@ -1,17 +1,76 @@
 require('dotenv').config();
+var _ = require('lodash');
+var Promise = require('bluebird');
+var Json2csvParser = require('json2csv').Parser;
+var fs = require('fs');
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device(process.env.INSTAGRAM_USER);
 var storage = new Client.CookieFileStorage(__dirname + '/cookies/someuser.json');
 
-// And go for login
-Client.Session.create(device, storage, process.env.INSTAGRAM_USER, process.env.INSTAGRAM_PASSWORD)
-	.then(function(session) {
-   		// Now you have a session, we can follow / unfollow, anything...
-		// And we want to follow Instagram official profile
-		return [session, Client.Account.searchForUser(session, 'saschafirtina')]   
-	})
-	.spread(function(session, account) {
-        var feed = Client.Feed.AccountFollowers(session, account.id,10);
+const exportFollowersData = function(instagramUser){
+    // And go for login
+    Client.Session.create(device, storage, process.env.INSTAGRAM_USER, process.env.INSTAGRAM_PASSWORD)
+        .then(function(session) {
+            // Now you have a session, we can follow / unfollow, anything...
+            // And we want to follow Instagram official profile
+            console.log('Getting data from the user');
+            return [session, Client.Account.searchForUser(session, instagramUser)]   
+        })
+        .spread(function(session, account) {
+            var feed = new Client.Feed.AccountFollowers(session, account.id,10);
+            console.log('Looking into the user followers');
+            Promise.mapSeries(_.range(0,5), function(){
+                return feed.get();
+            }).then(function(results){
+                var dataResult = [];
+                var columns = [
+                    'userId',
+                    'profilePicUrl',
+                    'username',
+                    'fullName',
+                    'isPrivate',
+                    'followerCount'
+                ];
 
-        console.log(feed);
-	}).then();
+                var promisesArray = [];
+                
+            _.forEach(results[0], function(data){
+                    var user = new Client.Account.searchForUser(session, data._params.username);
+                    promisesArray.push(user);
+
+                })
+
+                Promise.all(promisesArray).then(function(responseData){
+                    _.forEach(responseData, function(resp){
+                        var objectArray = {
+                            'userId': resp._params.id,
+                            'profilePicUrl': resp._params.profilePicUrl,
+                            'username': resp._params.username,
+                            'fullName': resp._params.fullName,
+                            'isPrivate': resp._params.isPrivate,
+                            'followerCount': resp._params.followerCount
+                        };
+                        dataResult.push(objectArray);
+                    })
+                    var toCsv = {
+                        data: dataResult,
+                        fields: columns,
+                        hasCSVColumnTitle: false  
+                    };
+                    console.log('Parsing data');
+                    var json2csvParser = new Json2csvParser({columns});
+                    var csv = json2csvParser.parse(dataResult);
+                    fs.writeFile('result.csv', csv, function(err){
+                        if (err) throw err;
+                        console.log('File created at: '+ __dirname+'\result.csv');
+                    })
+
+                })
+
+                
+                
+            })
+        })
+    }
+
+module.exports = {exportFollowersData};
